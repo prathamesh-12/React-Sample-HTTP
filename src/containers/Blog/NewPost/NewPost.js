98f08a7ff3a89249730a962964a3ddff43dacf09
Prathamesh V. Nimbkar <pratha662537@majesco.com>
2020-02-04 17:22:56 +0530

    Added Aux component

--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 import { Redirect } from 'react-router-dom';
 
 import './NewPost.css';
+import Aux from '../../../hoc/Auxillary/Aux';
+import Spinner from '../../../components/UI/Spinner/Spinner';
 
 class NewPost extends Component {
     state = {
@@ -36,29 +38,39 @@ class NewPost extends Component {
     render () {
         let redirectPost = null;
         let isError = null;
+        let newPostForm = null;
         if(this.state.isPostSuccessfullySaved) {
             redirectPost = (<Redirect to="/posts" />)
         }
         if(this.state.isError) {
             isError = (<Redirect to="/pageNotFound" />);
         }
+        if(!this.state.isLoading) {
+            newPostForm = (
+                <div className="NewPost">
+                    {redirectPost}
+                    {isError}
+                    <h1>Add a Post</h1>
+                    <label>Title</label>
+                    <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
+                    <label>Content</label>
+                    <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
+                    <label>Author</label>
+                    <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
+                        <option value="Max">John</option>
+                        <option value="Manu">Jack</option>
+                    </select>
+                    <button onClick={this.postDataHandler}>Add Post</button>
+                </div>
+            );
+        } else {
+            newPostForm = <Spinner />
+        }
 
         return (
-            <div className="NewPost">
-                {redirectPost}
-                {isError}
-                <h1>Add a Post</h1>
-                <label>Title</label>
-                <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
-                <label>Content</label>
-                <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
-                <label>Author</label>
-                <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
-                    <option value="Max">John</option>
-                    <option value="Manu">Jack</option>
-                </select>
-                <button onClick={this.postDataHandler}>Add Post</button>
-            </div>
+            <Aux>
+                {newPostForm}  
+            </Aux>
         );
     }
 }
