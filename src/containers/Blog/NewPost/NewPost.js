8f382e688f3c6213bf0de6bbb1ec6865860b9249
Prathamesh V. Nimbkar <pratha662537@majesco.com>
2020-02-04 17:28:23 +0530

    Code for managing spinner

--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -38,32 +38,30 @@ class NewPost extends Component {
     render () {
         let redirectPost = null;
         let isError = null;
-        let newPostForm = null;
+        let newPostForm = newPostForm = (
+            <div className="NewPost">
+                {redirectPost}
+                {isError}
+                <h1>Add a Post</h1>
+                <label>Title</label>
+                <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
+                <label>Content</label>
+                <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
+                <label>Author</label>
+                <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
+                    <option value="Max">John</option>
+                    <option value="Manu">Jack</option>
+                </select>
+                <button onClick={this.postDataHandler}>Add Post</button>
+            </div>);
+
         if(this.state.isPostSuccessfullySaved) {
             redirectPost = (<Redirect to="/posts" />)
         }
         if(this.state.isError) {
             isError = (<Redirect to="/pageNotFound" />);
         }
-        if(!this.state.isLoading) {
-            newPostForm = (
-                <div className="NewPost">
-                    {redirectPost}
-                    {isError}
-                    <h1>Add a Post</h1>
-                    <label>Title</label>
-                    <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
-                    <label>Content</label>
-                    <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
-                    <label>Author</label>
-                    <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
-                        <option value="Max">John</option>
-                        <option value="Manu">Jack</option>
-                    </select>
-                    <button onClick={this.postDataHandler}>Add Post</button>
-                </div>
-            );
-        } else {
+        if(this.state.isLoading) {
             newPostForm = <Spinner />
         }
 
