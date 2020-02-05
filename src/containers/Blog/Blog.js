cfbd88dd5668ffdf6d0a748092bf8359c4a0a703
Prathamesh V. Nimbkar <pratha662537@majesco.com>
2020-02-05 15:45:25 +0530

    Added async component with Lazy loading

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,9 +4,14 @@ import {Route, NavLink, Switch, Redirect} from 'react-router-dom';
 import './Blog.css';
 
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
-import FullPost from './FullPost/FullPost';
+//import NewPost from './NewPost/NewPost';
+//import FullPost from './FullPost/FullPost';
 import Error404 from '../../components/Error/Error-404/Error-404';
+import asyncComponent from '../../hoc/async/AsyncComponent';
+
+const AsyncNewPost = asyncComponent(() => {
+    return import("./NewPost/NewPost");
+})
 
 class Blog extends Component {
 
@@ -35,7 +40,7 @@ class Blog extends Component {
                     </nav>
                 </header>
                 <Switch>
-                    <Route path="/new-post" component={NewPost} />
+                    <Route path="/new-post" component={AsyncNewPost} />
                     <Route path="/posts" component={Posts} />
                     <Route path="/pageNotFound" component={Error404} />
                     <Redirect from="/" to="/posts"/>
--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -49,8 +49,8 @@ class NewPost extends Component {
                 <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
                 <label>Author</label>
                 <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
-                    <option value="Max">John</option>
-                    <option value="Manu">Jack</option>
+                    <option value="john">John</option>
+                    <option value="jack">Jack</option>
                 </select>
                 <button onClick={this.postDataHandler}>Add Post</button>
             </div>);
